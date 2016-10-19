e75962eb58b3b69551f22d605750b0fe1b84dd48
Todor Andreev <t.k.andreev@gmail.com>
2016-10-19 17:52:04 +0100

    change text input to voice recognizer from google

--- src/main/resources/webapp/source/voice_recognize.js
+++ src/main/resources/webapp/source/voice_recognize.js
@@ -1,4 +1,12 @@
-  function startDictation() {
+angular.module('zeroui',['ui.bootstrap'])
+
+angular.module('zeroui').controller('voice-converter',['$scope','$http',
+
+
+
+  function($scope, $http) {
+
+  $scope.submit = function() {
 
     if (window.hasOwnProperty('webkitSpeechRecognition')) {
 
@@ -14,7 +22,19 @@
         document.getElementById('transcript').value
                                  = e.results[0][0].transcript;
         recognition.stop();
-        document.getElementById('labnol').submit();
+        //document.getElementById('labnol').submit();
+
+var inputText = "https://api.projectoxford.ai/luis/v1/application?id=fd8fe0e7-2216-41ad-a509-c811c48baef3&subscription-key=fee7cf84355e468fac27b39957a0f8d6&"+document.getElementById('labnol');
+        $http({
+          method: 'GET',
+          url: inputText
+        }).then(function successCallback(response) {
+            alert("success");
+          }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            alert("error");
+          });
       };
 
       recognition.onerror = function(e) {
@@ -23,3 +43,5 @@
 
     }
   }
+}
+  ]);
