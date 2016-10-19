59d71641d9ae669c5e9db5a9b8ad204ad06f1888
cherry <1402486@qq.com>
2016-10-19 22:27:06 +0100

    submit when press enter in input box

--- src/main/resources/webapp/source/voice_recognize.js
+++ src/main/resources/webapp/source/voice_recognize.js
@@ -1,75 +1,89 @@
-angular.module('zeroui',['ui.bootstrap'])
+angular.module('zeroui', ['ui.bootstrap'])
 
-angular.module('zeroui').controller('voice-converter',['$scope','$http',
-
-
-
-  function($scope, $http) {
-
-                                     $scope.helpShown = false;
-                                      $scope.transferShown = false;
-                                      $scope.showmeShown = false;
-
-  $scope.submit = function() {
-
-    if (window.hasOwnProperty('webkitSpeechRecognition')) {
-
-      var recognition = new webkitSpeechRecognition();
-
-      recognition.continuous = false;
-      recognition.interimResults = false;
-
-      recognition.lang = "en-US";
-      recognition.start();
-
-      recognition.onresult = function(e) {
-        document.getElementById('transcript').value = e.results[0][0].transcript;
-        recognition.stop();
-
-var inputText = 'https://api.projectoxford.ai/luis/v1/application?id=fd8fe0e7-2216-41ad-a509-c811c48baef3&subscription-key=fee7cf84355e468fac27b39957a0f8d6&q='+document.getElementById('transcript').value;
-          $http.get(inputText)
-              .success(function (data) {
-
-                  $scope.result  = data;
-                  var result = data.intents[0];
-
-                  if(result.intent == 'Transfer'){
-                     $scope.helpShown = false;
-                     $scope.transferShown = true;
-                     $scope.showmeShown = false;
-                     $scope.transfer = result.actions[0].parameters;
+angular.module('zeroui').controller('voice-converter', ['$scope', '$http',
 
 
-                  } else if(result.intent == 'Show Me'){
-                     $scope.helpShown = false;
-                     $scope.transferShown = false;
-                     $scope.showmeShown = true;
+    function ($scope, $http) {
 
-                  } else if(result.intent == 'Help'){
-                    $scope.helpShown = true;
-                    $scope.transferShown = false;
-                    $scope.showmeShown = false;
-                    $scope.help = "Tips: Transfer $100 from Sara To Cherry.";
+        $scope.helpShown = false;
+        $scope.transferShown = false;
+        $scope.showmeShown = false;
 
-                  } else if(result.intent == 'How Much'){
+        $scope.submit = function () {
 
-                  } else{
-                    alert('error intent!');
-                  }
+            if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+                var recognition = new webkitSpeechRecognition();
+
+                recognition.continuous = false;
+                recognition.interimResults = false;
+
+                recognition.lang = "en-US";
+                recognition.start();
+
+                recognition.onresult = function (e) {
+                    document.getElementById('transcript').value = e.results[0][0].transcript;
+                    recognition.stop();
+                    $scope.restCall();
 
 
+                };
+                $scope.restCall=function(){
+                    var inputText = 'https://api.projectoxford.ai/luis/v1/application?id=fd8fe0e7-2216-41ad-a509-c811c48baef3&subscription-key=fee7cf84355e468fac27b39957a0f8d6&q=' + document.getElementById('transcript').value;
+                    $http.get(inputText)
+                        .success(function (data) {
 
-              }).error(function (response) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-          });
-      };
+                            $scope.result = data;
+                            var result = data.intents[0];
 
-      recognition.onerror = function(e) {
-        recognition.stop();
-      }
+                            if (result.intent == 'Transfer') {
+                                $scope.helpShown = false;
+                                $scope.transferShown = true;
+                                $scope.showmeShown = false;
+                                $scope.transfer = result.actions[0].parameters;
 
+
+                            } else if (result.intent == 'Show Me') {
+                                $scope.helpShown = false;
+                                $scope.transferShown = false;
+                                $scope.showmeShown = true;
+
+                            } else if (result.intent == 'Help') {
+                                $scope.helpShown = true;
+                                $scope.transferShown = false;
+                                $scope.showmeShown = false;
+                                $scope.help = "Tips: Transfer $100 from Sara To Cherry.";
+
+                            } else if (result.intent == 'How Much') {
+
+                            } else {
+                                alert('error intent!');
+                            }
+
+
+                        }).error(function (response) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                    });
+                }
+                recognition.onerror = function (e) {
+                    recognition.stop();
+                }
+
+            }
+        }
     }
-  }
-}
-  ]);
+]);
+angular.module('zeroui').directive('myEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.myEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
